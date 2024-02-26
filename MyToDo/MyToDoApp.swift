1c2e9a7815110b9a78d4fd862902b92b27cd82ee
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-02-26 16:07:57 +0100

    Initial Commit

---
+++ MyToDo/MyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoApp.swift
+//  MyToDo
+//
+//  Created by Godwin IE on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
